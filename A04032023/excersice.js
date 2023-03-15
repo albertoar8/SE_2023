6fdaef070add53271e8c8db3ed0433702da8d983
albertoar8 <albertoar8@gmail.com>
2023-03-15 20:50:44 +1100

    test9500

--- A04032023/excersice.js
+++ A04032023/excersice.js
@@ -1,14 +1,63 @@
 
-//Exercise 1: Get the total budget of all movies - need to inogre the tbd movie (Easy)
+//Loading JSON file
+const movieJSON = require("./movie.json");
 
+/*
+  Hint 1: Get all keys of the marvelMovie - object 
+  (movie name) and store them in the array - Object.keys()
+*/
+const marvelMovie = movieJSON["Marvel Cinematic Universe"];
+const movieNameArr = Object.keys(marvelMovie);
+
+//Exercise 1: Get the total budget of all movies need to inogre the tbd movie (Easy)
+const totalBudgetExercise1 = calculateTotalBudget(marvelMovie, movieNameArr);
+//console.log(totalBudgetExercise1);
 //Exercise 2: Get the list of action movie (category_name) (Middle)
+const listActionMovie = getListOfActionMovie(marvelMovie, movieNameArr);
+console.log(listActionMovie);
+//console.log(listActionMovie);
 
 //Exercise 3: Get all the movie that has the budget > 175.000.000 - need to inogre the tbd movie (Easy)
-const moviejson=require('./movie.json');
-//console.log(moviejson);
+function gethigherbudget(movieJSON,movieNameArr){
+    
+}
 
-const marvelMovie = moviejson['Marvel Cinematic Universe']
-//console.log(marvelMovie);
+/**
+ * Method to calculate the total budget
+ * 2 params : the JSON movie object and the array of all movie name
+ */
+function calculateTotalBudget(movieJSON, movieNameArr) {
+  let totalBudget = 0;
+  for (let i = 0; i < movieNameArr.length; i++) {
+    //Get value of each movie
+    const movieName = movieNameArr[i];
+    const movie = movieJSON[movieName];
 
-let moviename=Object.keys(marvelMovie);
-console.log(moviename);
+    const budget = movie.budget;
+    if (budget.toLowerCase() !== "tbd") {
+      totalBudget = totalBudget + convertCurrencyToNumber(budget);
+    }
+  }
+  return totalBudget;
+}
+
+function getListOfActionMovie(movieJSON, movieNameArr) {
+  const listActionMovie = [];
+  for(let i = 0; i<movieNameArr.length; i++) {
+    //Get value of each movie
+    const movieName = movieNameArr[i];
+    const movie = movieJSON[movieName];
+
+    const category = movie.category_name; 
+    if (category && category.toLowerCase() === 'action') {
+      //Push them into a list
+      listActionMovie.push(movie);
+    }
+  }
+  return listActionMovie;
+}
+
+function convertCurrencyToNumber(item) {
+  const convertItem = item.replaceAll(",", ""); //"200,000,000" -> "20000000"
+  return parseInt(convertItem); //"20000000" => 200000000
+}
