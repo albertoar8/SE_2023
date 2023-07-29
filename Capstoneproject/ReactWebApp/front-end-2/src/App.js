1958fbb6bf7658be09b31529db3f42d5c1677146
albertoar8 <albertoar8@gmail.com>
2023-07-29 16:04:06 +1000

    latest updates

--- Capstoneproject/ReactWebApp/front-end-2/src/App.js
+++ Capstoneproject/ReactWebApp/front-end-2/src/App.js
@@ -3,24 +3,42 @@ import "./App.css";
 import { BrowserRouter} from "react-router-dom";
 import { AppRoutes } from "./Routes/routes";
 import Login from '../src/Components/Login/Login.js';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
 
 function App() {
-  const [token, setToken] = useState();
-  if(!token) {
-    return (
-      <BrowserRouter>
-        <Login setToken={setToken} />
-      </BrowserRouter>
-    )
-  }
-  return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
-    </div>
-  );
+  // const [token, setToken] = useState();
+  // if(!token) {
+  //   return (
+  //     <BrowserRouter>
+  //       <Login setToken={setToken} />
+  //     </BrowserRouter>
+  //   )
+  // }
+  // return (
+  //   <div className="wrapper">
+  //     <h1>Application</h1>
+  //     <BrowserRouter>
+  //       <AppRoutes/>
+  //     </BrowserRouter>
+  //   </div>
+  // );
+/*--------------------------------------------------*/
+return (
+  <div className="wrapper">
+    <h1>Application</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 }
 
 export default App;
--- Capstoneproject/ReactWebApp/front-end-2/src/Routes/routes.js
+++ Capstoneproject/ReactWebApp/front-end-2/src/Routes/routes.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "../Components/Dashboard/Dashboard";
-import Preferences from "../Components/Preferences/Preferences";
+import Home from "../Components/Home/Home";
+import About from "../Components/About/About";
+import Book from "../Components/Book/Book";
+import Search from "../Components/Search/Search";
 import Login from "../Components/Login/Login";
 
 export const AppRoutes = (props) => {
   return (
     <Routes>
-      <Route path="/" element={<Login {...props} />} />
-      <Route path="/dashboard" element={<Dashboard {...props} />} />
-      <Route path="/preferences" element={<Preferences {...props} />} />
+      <Route path="/" element={<Home {...props} />} />
+      <Route path="/about" element={<About {...props} />} />
+      <Route path="/search" element={<Search {...props} />} />
+      <Route path="/book" element={<Book {...props} />} />
       <Route path="/login" element={<Login {...props} />} />
     </Routes>
   );
